7b0070433fa814865604c6ea435451e23e6626ef
DEF4LT-303 <ryanrafi72@gmail.com>
2023-10-21 13:42:48 +0600

    footer content

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './globals.css';
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         <main className='app'>
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>

e55ce6f669e76fe479c76a67aacae58b521b0dc2
Ryan Rafi <ryanrafi72@gmail.com>
2023-11-23 00:04:42 +0600

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,21 +1,18 @@
-'use client';
-
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser
-//     .accessToken;
-
-const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
 });
 
-export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  headers: { token: `${TOKEN}` }
-});
+export const userRequest = () => {
+  const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
+  const currentUser = user && JSON.parse(user).currentUser;
+  const TOKEN = currentUser?.accessToken;
+
+  return axios.create({
+    baseURL: BASE_URL,
+    headers: { token: `${TOKEN}` }
+  });
+};

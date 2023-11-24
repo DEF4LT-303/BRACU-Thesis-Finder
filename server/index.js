3aaed90b0386ac8c56045b9a173a2ee439ff0df9
DEF4LT-303 <ryanrafi72@gmail.com>
2023-11-24 20:28:34 +0600

    fixed errors

--- server/index.js
+++ server/index.js
@@ -1,31 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-const chatRoutes = require("./routes/chatRoutes")
-const messageRoutes = require("./routes/messageRoutes")
-const connectDB = require('./config/db')
-const colors = require('colors')
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
-
-connectDB()
 app.use(cors());
 
 dotenv.config();
 app.get('/', (req, res) => {
-  res.send("API is running")
-})
-// mongoose
-//   .connect(process.env.DB_CONNECT)
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch((err) => console.log(err));
+  res.send('API is running');
+});
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
-app.use('/api/chat', chatRoutes)
-app.use('/api/message', messageRoutes)
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
-const PORT = process.env.PORT || 5000 
-
-const server = app.listen(PORT, console.log(`server Started on PORT ${PORT}`.yellow.bold))
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);

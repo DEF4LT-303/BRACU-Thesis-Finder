48bf8e235e039b45a95b8547262e416b625a0e13
DEF4LT-303 <ryanrafi72@gmail.com>
2023-11-30 13:34:50 +0600

    added post controller todo for group

---
+++ server/controllers/postController.js
@@ -0,0 +1,5 @@
+// TODO - Add create post controller
+// TODO - Add getAll post controller
+// TODO - Add get post controller
+// TODO - Add update post controller
+// TODO - Add delete post controller

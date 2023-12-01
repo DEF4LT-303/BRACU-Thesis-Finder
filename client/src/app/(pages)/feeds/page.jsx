3378d07246adb81b2c9e2feed0af3a5342a910a2
DEF4LT-303 <ryanrafi72@gmail.com>
2023-12-01 19:48:31 +0600

    skeleton loader

--- client/src/app/(pages)/feeds/page.jsx
+++ client/src/app/(pages)/feeds/page.jsx
@@ -1,5 +1,9 @@
 import Card from '@/app/components/Card';
 
+// TODO - Initial post is public [Phase 1]
+// TODO - On required users, post become private after creating GC [Move to phase 2]
+// TODO - Author can finalize physical meeting date by clicking the next button [Move to phase 3]
+// TODO - Author can click finalize button after meeting to complete the post [Move to phase 4]
 const feeds = [
   {
     id: 1,
@@ -7,7 +11,8 @@ const feeds = [
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis tincidunt ultricies, nunc nisl ultricies nunc, vitae aliqua',
     createdAt: 'Mar 20, 2023',
     tags: ['Design', 'Accessibility', 'UI', 'Testing'],
-    phase: 4,
+    phase: 3,
+    isPublic: true,
     author: {
       name: 'John Doe',
       avatar:
@@ -35,6 +40,7 @@ const feeds = [
     createdAt: 'Mar 20, 2023',
     tags: ['Design', 'Accessibility'],
     phase: 2,
+    isPublic: true,
     author: {
       name: 'John Doe',
       avatar:
@@ -54,8 +60,7 @@ const feeds = [
         },
         {
           name: 'Jane Doe',
-          avatar:
-            'https://i.pinimg.com/originals/12/25/7c/12257c071a178a1bbe6ffab5863356cf.jpg'
+          avatar: 'default.jpg'
         }
       ]
     }
@@ -65,7 +70,8 @@ const feeds = [
 const Feeds = () => {
   return (
     <div className='bg-base-100 min-h-full'>
-      <div className='flex flex-col items-center py-10 px-4 md:px-10'>
+      <h2 className='text-3xl font-bold text-center my-5'>Feeds</h2>
+      <div className='flex flex-col items-center md:px-10'>
         {feeds.map((feed) => (
           <Card feed={feed} />
         ))}

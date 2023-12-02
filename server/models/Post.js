fe447e7e4d7998dab4452e124d740a4ecbc72d18
DEF4LT-303 <ryanrafi72@gmail.com>
2023-12-02 22:57:12 +0600

    fixes on post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,32 +1,48 @@
 const mongoose = require('mongoose');
 
-const postModel = new mongoose.Schema({
+const postModel = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: [true, 'Title is required'],
-        trim: true,
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true
     },
 
-    description: {
-        type: String,
-        required: [true, 'Description is required'],
-        trim: true,
+    desc: {
+      type: String,
+      required: [true, 'Description is required'],
+      trim: true
     },
 
     author: {
-        type: String,
-        // ref: 'users', // Assuming your user model is named 'User'
-        required: [true, 'Author is required'],
+      type: String,
+      ref: 'User', // Assuming your user model is named 'User'
+      required: [true, 'Author is required']
     },
 
     tags: {
-        type: [String],
-        default: [],
+      type: Array,
+      default: []
     },
-},
-    {
-        timestamps: true
+
+    phase: {
+      type: Number,
+      default: 1
+    },
+
+    isPublic: {
+      type: Boolean,
+      default: true
+    },
+
+    applied: {
+      type: Array,
+      default: []
     }
+  },
+  {
+    timestamps: true
+  }
 );
 
 const Post = mongoose.model('Post', postModel);

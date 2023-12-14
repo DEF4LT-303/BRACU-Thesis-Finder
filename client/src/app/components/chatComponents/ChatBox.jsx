829ed6d8876cd8de1c978e6bf99fcf007c2b2b1c
Arman <fakidarman@gmail.com>
2023-12-14 21:51:29 +0600

    chat ui styling redering solved

--- client/src/app/components/chatComponents/ChatBox.jsx
+++ client/src/app/components/chatComponents/ChatBox.jsx
@@ -1,12 +1,20 @@
 import { ChatState } from '@/app/ChatProvider'
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const ChatBox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = ChatState()
   return (
-    <div>
-      This is ChatBox
-    </div>
+    <Box d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px">
+
+    </Box>
   )
 }
 
